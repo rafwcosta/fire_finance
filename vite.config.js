3aeb55e18bf43d1351b35331b92ecb30d9a5407a
rafwcosta <zrafaelcosta@gmail.com>
2026-05-10 09:03:05 -0300

    atualizações

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Troque 'gestor-financeiro' pelo nome EXATO do seu repositório no GitHub
 export default defineConfig({
-    plugins: [react()],
-    base: '/fire_finance/',
+  plugins: [react()],
+  base: '/gestor-financeiro/',
 })
